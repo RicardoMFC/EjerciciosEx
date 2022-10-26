0f53f9c70194550fa5b7e6c5da98c17df4c63def
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-26 20:37:03 +0200

    e

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -3,12 +3,18 @@ import sys
 def lista():
     cadena="Un día el viento soplaba con fuerza#mira cómo se mueve aquella banderola - dijo un monje#lo que se mueve es el viento - respondió otromonje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes - dijo el maestro"
     aux=[]
+    aux2=[]
     #aux=cadena.replace('#','\n')
+    j=0
     for i in range(len(cadena)):
         if cadena[i]=='#':
-            cadena.insert(i-1,".")
-            aux=cadena.replace('#','\n')
-            aux[i+1].upper()    
+            
+            #cadena.insert(i-1,".")
+            aux=cadena.replace('#','.\n-')
+            aux.replace(i+1,aux.upper())
+            
+
+            
     return aux
 
 def main():
---
+++ ej9.py
@@ -0,0 +1,6 @@
+lista=['h','e','y','u']
+lista.append('h')
+print(lista)
+
+
+
